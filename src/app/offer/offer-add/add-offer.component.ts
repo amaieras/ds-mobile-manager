7a1e4f15b0f7afbe1c40660c50c8b5bc5152853a
amaieras <maieras.andrei@gmail.com>
2018-02-06 17:04:55 +0200

    [Dev] Work on offer functionality

---
+++ src/app/offer/offer-add/add-offer.component.ts
@@ -0,0 +1,14 @@
+import {Component, OnInit} from "@angular/core";
+
+@Component({
+  selector: 'app-option-offer-list',
+  templateUrl: './offer-option-list.component.html'
+})
+export class OfferOptionListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
