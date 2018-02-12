5f39e46ce5c17fd8e6970ad43b2eb9401393aee9
amaieras <maieras.andrei@gmail.com>
2018-02-12 10:04:23 +0200

    Set db to dev env

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -46,7 +46,7 @@ export const environment = {
     messagingSenderId: '662623055885'
   }
 }
-export const firebaseConfig = environment.firebaseConfigProd;
+export const firebaseConfig = environment.firebaseConfig;
 
 @NgModule({
     declarations: [
